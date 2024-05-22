2c8a3acd80ff806168d1a483635419eefd57225f
bac0nb0yy <kyuma@pm.me>
2024-05-22 10:26:58 +0200

    directory ex11 added

---
+++ ex11/src/vector.rs
@@ -0,0 +1,219 @@
+use crate::field::*;
+
+use std::fmt::{Display, Formatter, Result};
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
+
+#[derive(Debug, Clone)]
+pub struct Vector<K, const N: usize> {
+    data: [K; N],
+    dim: usize,
+}
+
+impl<K: Field + Display, const N: usize> Display for Vector<K, N> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "[")?;
+        for (index, item) in self.data.iter().enumerate() {
+            if index != 0 {
+                write!(f, ", ")?;
+            }
+            write!(f, "{:.3}", item)?;
+        }
+        writeln!(f, "]")?;
+        Ok(())
+    }
+}
+
+impl<K: Field, const N: usize> Add<Vector<K, N>> for Vector<K, N> {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let mut data = [K::zero(); N];
+        for i in 0..N {
+            data[i] = self.data[i] + rhs.data[i];
+        }
+
+        Vector { data, dim: N }
+    }
+}
+
+impl<K: Field, const N: usize> Add<K> for Vector<K, N> {
+    type Output = Self;
+
+    fn add(self, scalar: K) -> Self::Output {
+        Vector {
+            data: self.data.map(|a| a + scalar),
+            dim: N,
+        }
+    }
+}
+
+impl<K: Field, const N: usize> AddAssign<Vector<K, N>> for Vector<K, N> {
+    fn add_assign(&mut self, rhs: Vector<K, N>) {
+        self.data
+            .iter_mut()
+            .zip(&rhs.data)
+            .for_each(|(a, &b)| *a += b);
+    }
+}
+
+impl<K: Field, const N: usize> AddAssign<K> for Vector<K, N> {
+    fn add_assign(&mut self, rhs: K) {
+        self.data.iter_mut().for_each(|a| *a += rhs);
+    }
+}
+
+impl<K: Field, const N: usize> Sub<Vector<K, N>> for Vector<K, N> {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        let mut data = [K::zero(); N];
+        for i in 0..N {
+            data[i] = self.data[i] - rhs.data[i];
+        }
+
+        Vector { data, dim: N }
+    }
+}
+
+impl<K: Field, const N: usize> Sub<K> for Vector<K, N> {
+    type Output = Self;
+
+    fn sub(self, scalar: K) -> Self::Output {
+        Vector {
+            data: self.data.map(|a| a - scalar),
+            dim: N,
+        }
+    }
+}
+
+impl<K: Field, const N: usize> SubAssign<Vector<K, N>> for Vector<K, N> {
+    fn sub_assign(&mut self, rhs: Vector<K, N>) {
+        self.data
+            .iter_mut()
+            .zip(&rhs.data)
+            .for_each(|(a, &b)| *a -= b);
+    }
+}
+
+impl<K: Field, const N: usize> SubAssign<K> for Vector<K, N> {
+    fn sub_assign(&mut self, scalar: K) {
+        self.data.iter_mut().for_each(|a| *a -= scalar);
+    }
+}
+
+impl<K: Field, const N: usize> Mul<Vector<K, N>> for Vector<K, N> {
+    type Output = K;
+
+    fn mul(self, rhs: Vector<K, N>) -> K {
+        self.dot(&rhs)
+    }
+}
+
+impl<K: Field, const N: usize> Mul<K> for Vector<K, N> {
+    type Output = Self;
+
+    fn mul(self, scalar: K) -> Self::Output {
+        Vector {
+            data: self.data.map(|a| a * scalar),
+            dim: N,
+        }
+    }
+}
+
+impl<K: Field, const N: usize> MulAssign<Vector<K, N>> for Vector<K, N> {
+    fn mul_assign(&mut self, rhs: Vector<K, N>) {
+        self.data
+            .iter_mut()
+            .zip(&rhs.data)
+            .for_each(|(a, &b)| *a *= b);
+    }
+}
+
+impl<K: Field, const N: usize> MulAssign<K> for Vector<K, N> {
+    fn mul_assign(&mut self, scl: K) {
+        self.data.iter_mut().for_each(|a| *a *= scl);
+    }
+}
+
+impl<K: Field, const N: usize> Div<Vector<K, N>> for Vector<K, N> {
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        let mut data = [K::zero(); N];
+        for i in 0..N {
+            data[i] = self.data[i] / rhs.data[i];
+        }
+
+        Vector { data, dim: N }
+    }
+}
+
+impl<K: Field, const N: usize> Div<K> for Vector<K, N> {
+    type Output = Self;
+
+    fn div(self, scalar: K) -> Self::Output {
+        Vector {
+            data: self.data.map(|a| a / scalar),
+            dim: N,
+        }
+    }
+}
+
+impl<K: Field, const N: usize> DivAssign<Vector<K, N>> for Vector<K, N> {
+    fn div_assign(&mut self, rhs: Vector<K, N>) {
+        self.data
+            .iter_mut()
+            .zip(&rhs.data)
+            .for_each(|(a, &b)| *a /= b);
+    }
+}
+
+impl<K: Field, const N: usize> DivAssign<K> for Vector<K, N> {
+    fn div_assign(&mut self, scalar: K) {
+        self.data.iter_mut().for_each(|a| *a /= scalar);
+    }
+}
+
+impl<K: Field, const N: usize> Neg for Vector<K, N> {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Vector {
+            data: self.data.map(|x| -x),
+            dim: self.dim,
+        }
+    }
+}
+
+impl<K: Field, const N: usize> Vector<K, N> {
+    pub fn dot(&self, v: &Vector<K, N>) -> K {
+        self.data
+            .iter()
+            .zip(&v.data)
+            .fold(K::zero(), |acc, (&x, &y)| acc + x * y)
+    }
+
+    pub fn new(data: [K; N]) -> Self {
+        let dim: usize = data.len();
+        Vector { data, dim }
+    }
+
+    pub fn data(&self) -> &[K; N] {
+        &self.data
+    }
+
+    pub fn data_mut(&mut self) -> &mut [K; N] {
+        &mut self.data
+    }
+
+    #[allow(dead_code)]
+    pub fn dim(&self) -> usize {
+        self.dim
+    }
+}
+
+impl<K: Field, const N: usize> From<[K; N]> for Vector<K, N> {
+    fn from(array: [K; N]) -> Self {
+        Vector::new(array)
+    }
+}
