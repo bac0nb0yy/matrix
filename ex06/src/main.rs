02fe836e36ff2c91e41f11bfa70242e10e99c08c
bac0nb0yy <kyuma@pm.me>
2024-05-22 15:47:43 +0200

    ex06 added more tests

--- ex06/src/main.rs
+++ ex06/src/main.rs
@@ -4,8 +4,8 @@ mod vector;
 use field::*;
 use vector::Vector;
 
-fn cross_product_3<K: Field>(u: &Vector<K, 3>, v: &Vector<K, 3>) -> Vector<K, 3> {
-    Vector::new(vec![
+fn cross_product<K: Field>(u: &Vector<K, 3>, v: &Vector<K, 3>) -> Vector<K, 3> {
+    Vector::new([
         u.data()[1] * v.data()[2] - u.data()[2] * v.data()[1],
         u.data()[2] * v.data()[0] - u.data()[0] * v.data()[2],
         u.data()[0] * v.data()[1] - u.data()[1] * v.data()[0],
@@ -16,23 +16,31 @@ fn cross_product_3<K: Field>(u: &Vector<K, 3>, v: &Vector<K, 3>) -> Vector<K, 3>
 mod tests {
     use super::*;
     use approx::assert_abs_diff_eq;
-    use nalgebra::DVector;
+    use nalgebra::SVector;
     use rand::prelude::*;
 
     const NB_TESTCASE_VECTORS: usize = 100;
     const THRESHOLD: f64 = 1e-10;
 
-    fn test_cross_product(size: usize) {
+    fn test_cross_product() {
         for _ in 0..NB_TESTCASE_VECTORS {
             let mut rng = rand::thread_rng();
-            let v1 = Vec::<f64>::from_iter((0..size).map(|_| rng.gen::<f64>()));
-            let v2 = Vec::<f64>::from_iter((0..size).map(|_| rng.gen::<f64>()));
+            let v1: [f64; 3] = (0..3)
+                .map(|_| rng.gen())
+                .collect::<Vec<_>>()
+                .try_into()
+                .unwrap();
+            let v2: [f64; 3] = (0..3)
+                .map(|_| rng.gen())
+                .collect::<Vec<_>>()
+                .try_into()
+                .unwrap();
 
             let my_v1 = Vector::new(v1);
             let my_v2 = Vector::new(v2);
 
-            let nalgebra_v1 = DVector::<f64>::from_vec(my_v1.data().clone());
-            let nalgebra_v2 = DVector::<f64>::from_vec(my_v2.data().clone());
+            let nalgebra_v1 = SVector::<f64, 3>::from_vec(v1.to_vec());
+            let nalgebra_v2 = SVector::<f64, 3>::from_vec(v2.to_vec());
 
             let my_cross_product = cross_product(&my_v1, &my_v2);
             let nalgebra_cross_product = nalgebra_v1.cross(&nalgebra_v2);
@@ -44,10 +52,8 @@ mod tests {
     }
 
     #[test]
-    fn test_cross_product_vector3() {
-        for _ in 0..NB_TESTCASE_VECTORS {
-            test_cross_product(3);
-        }
+    fn test_cross_product_vector() {
+        test_cross_product();
     }
 }
 
